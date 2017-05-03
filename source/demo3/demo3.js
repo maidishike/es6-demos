b9cea1ce36caf0651d45fd1c4e2f9af3d9a1228f
Toby <wsgtracy@163.com>
2017-05-03 10:06:56 +0800

    模板字符串-Template String

---
+++ source/demo3/demo3.js
@@ -0,0 +1,26 @@
+// 模板字符串
+
+console.log('模板字符串');
+let name = 'Bob', time = 'today';
+let str = `Hello ${name}, how are you ${time}`;
+console.log(str); // Hello Bob, how are you today
+
+let x = 1, y = 2;
+console.log(`${x} + ${y} = ${x + y}`); // 1 + 2 = 3
+console.log(`${x} + ${y * 2} = ${x + y * 2}`); // 1 + 4 = 5
+
+var obj = {x: 1, y: 2};
+console.log(`${obj.x + obj.y}`); // 3
+
+function fn () {
+  return 'Hello World';
+}
+console.log(`${fn()}`); // Hello World
+
+// 变量place没有声明
+console.log('');
+console.log('变量place没有声明');
+// var msg = `Hello, ${place}`;
+// console.log(msg); // 报错 ReferenceError: place is not defined
+
+console.log(`Hello ${'World'}`); // Hello World
